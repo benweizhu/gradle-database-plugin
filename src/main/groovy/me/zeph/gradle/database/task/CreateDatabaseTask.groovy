2001c622b4b653be58e79adce756fd278eb0a481
benweizhu <benweizhu@gmail.com>
2015-03-15 22:34:28 +0800

    [gradle-plugin-database][benwei] format

--- src/main/groovy/me/zeph/gradle/database/task/CreateDatabaseTask.groovy
+++ src/main/groovy/me/zeph/gradle/database/task/CreateDatabaseTask.groovy
@@ -10,8 +10,7 @@ import java.sql.DriverManager
 
 class CreateDatabaseTask extends DefaultTask {
 
-
-    public static final String CONFIGURATION_NAME = 'driver'
+    static final String CONFIGURATION_NAME = 'driver'
 
     @TaskAction
     def createDatabase() {
